e616ff8b3548fe7d041ce756fb67d6f21b0b334c
Zhanna <zhanna.libman@gmail.com>
2016-09-20 10:59:28 +0300

    Initial Commit

---
+++ My Shopping List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Shopping List
+//
+//  Created by Zhanna Libman on 20/09/2016.
+//  Copyright © 2016 Zhanna Libman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
